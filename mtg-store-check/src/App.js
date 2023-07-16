02277f8385d75c650ee97977beacb841602b5900
khoeye <kyle.hoeye@gmail.com>
2023-07-15 22:39:46 -0500

    Intial setup
    All the first  stuff

---
+++ mtg-store-check/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState} from 'react'
+import submissionForm from './components/submissionForm/submissionForm';
+
+function App() {
+  return (
+    <div className="App">
+      <submissionForm></submissionForm>
+    </div>
+  );
+}
+
+export default App;
