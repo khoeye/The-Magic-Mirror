0031f88e77ef82a82b3f2ac7edb6ef3000a086ea
khoeye <kyle.hoeye@gmail.com>
2024-01-05 22:29:29 -0600

    Added Fetch api for Scryfall

--- mtg-store-check/src/components/submissionForm/searchBar.js
+++ mtg-store-check/src/components/submissionForm/searchBar.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import sleep from "sleep-promise";
+import axios from "axios";
 
 const SearchBar = ({setResults}) => {
     const [cardImport, setCardImport] = useState("");
@@ -9,24 +10,56 @@ const SearchBar = ({setResults}) => {
       setSubmitClicked(true);
       event.preventDefault();
     };
-  
+
     const inputHandler = (value) => {
-      setCardImport(value);
-      fetchData(value);
-    }
-  
-    const fetchData = (value) => {
-      setCardImport(value);
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((json) => {
-          const results = json.filter((user) =>
-          {return value && user && user.name && user.name.toLowerCase().includes(value)}
-          ) 
-          setResults(results)
-        })
-        .then(sleep(300))
+      const params = {
+        q: value,
     };
+    console.log(getData(value))
+      setCardImport(value);
+      // fetchData(value);
+    }
+
+async function getData(value) {
+  
+  const response = await fetch('https://api.scryfall.com/cards/search?' + new URLSearchParams({
+    q: value.toString(),
+}))
+sleep(2000)
+return response.json(); // parses JSON response into native JavaScript objects
+  
+};
+
+
+    // const fetchData = (value) => {
+    //   setCardImport(value);
+    //   axios.get('https://api.scryfall.com/cards/search', {
+    //     params: {
+    //       q: cardImport
+    //     }
+    //   }, {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json',
+    //       'Host': 'http://localhost:3000/'
+    //     }
+    // })
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //       const results = json.data((name) =>
+    //       {return results}
+    //       ) 
+    //       setResults(results)
+    //     })
+    //     .then(sleep(300))
+    // };
+
+    // async function logMovies() {
+    //   const response = await fetch("https://api.scryfall.com/cards/search?q=magic");
+    //   const movies = await response.json();
+    //   console.log(movies);
+    // }
+    
 
     return (
     <Fragment>
