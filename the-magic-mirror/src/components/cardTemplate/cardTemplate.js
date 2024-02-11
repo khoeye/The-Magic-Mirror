ebaa2a5a718a604c93d57c2e5bc97ba446e4ee0a
khoeye <kyle.hoeye@gmail.com>
2024-02-11 15:33:02 -0600

    add some initial styling

--- the-magic-mirror/src/components/cardTemplate/cardTemplate.js
+++ the-magic-mirror/src/components/cardTemplate/cardTemplate.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
 import sleep from "sleep-promise";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import Hero from "../hero/hero";
-
+import styles from "./cardTemplate.module.css"
 
 const CardTemplate = () => {
     const { state } = useLocation()
-    console.log(state)
-    // eslint-disable-next-line no-unused-vars
+    const [passedHomeCard, setPassedHomeCard] = useState({
+        selectedCard: "The Meep"
+    })
+    console.log(passedHomeCard.selectedCard)
     async function getResponse(value) {
         const url = `https://api.scryfall.com/cards/named?exact=${value}`
           const response = await fetch(url);
@@ -17,11 +21,18 @@ const CardTemplate = () => {
         return data
       }
 
-
     return(
         <>
-        <Hero />
-        <button></button>
+       
+        <div className={styles.header}>
+             <Hero />
+             <div className={styles.headerText}>Highlight text and select &quot;Extract&quot; for it to be added to the search.</div>
+        </div>
+        <div className={styles.dataContainer}>
+            <div className={styles.leftContainer}></div>
+            <div className={styles.rightContainer}></div>
+        </div>
+        <button className={styles.buttonTemp} onClick={getResponse(passedHomeCard.selectedCard)}></button>
         </>
     )
 }
