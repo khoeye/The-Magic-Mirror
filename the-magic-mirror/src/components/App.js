c705ebc1465253d5a1093a2bddb5fc0fca23c439
khoeye <kyle.hoeye@gmail.com>
2024-07-06 16:58:39 -0500

    rework  react router setup and add store.js

---
+++ the-magic-mirror/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './home/homePage';
+import '../index.css';
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+
+export default App
