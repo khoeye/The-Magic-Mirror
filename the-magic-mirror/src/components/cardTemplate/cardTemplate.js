2369150fdcb08820d2bb44cf1a5a1cfc3a54fe0e
khoeye <kyle.hoeye@gmail.com>
2024-10-13 13:00:59 -0500

    Add logic for extraction of selectedText

--- the-magic-mirror/src/components/cardTemplate/cardTemplate.js
+++ the-magic-mirror/src/components/cardTemplate/cardTemplate.js
@@ -2,7 +2,7 @@
 import React from "react";
 import sleep from "sleep-promise";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./cardTemplate.module.css";
 import CardDetailsObject from "./cardDetailsObject";
 import { parseSpaceFunction } from "./helper/textFormater";
@@ -14,7 +14,8 @@ const CardTemplate = () => {
     selectedCard: "The Meep",
   });
   const [cardDetails, setCardDetails] = useState(CardDetailsObject);
-  const [extractedText, setExtractedText] = useState({})
+  const [selectedText, setSelectedText] = useState(undefined)
+  const [extractedValues, setExtractedValues] = useState([])
   const [isTextSelected, setIsTextSelected] = useState(false)
   const [extractedPosition, setExtractedPosition] = useState({})
 
@@ -22,79 +23,99 @@ const CardTemplate = () => {
     const url = `https://api.scryfall.com/cards/named?exact=${value}`;
     const response = await fetch(url);
     const data = await response.json();
+    //This is the recommended delay for calls into scryfall. 
     sleep(500);
-    console.log(data);
     return data;
   }
-const cancelSelection = () =>{
-  setIsTextSelected(false)
 
+  const extractText = async () => {
+    setExtractedValues(prevState => ([...prevState, selectedText]));
+  }
+const handleExtractClick = async () => {
+  await extractText()
+  setIsTextSelected(false);
+  setSelectedText(undefined);
 }
-  const handleSelection = () => {
-    let selectedText = window.getSelection().toString();
-    let selectedTextCategory = window.getSelection().baseNode.parentNode.id.toString()
-    setExtractedText({...extractedText,
-      [selectedTextCategory]: selectedText})
+
+const handleSelection = () => {
+    let getText = window.getSelection().toString();
+    let getTextCategory = window.getSelection().baseNode.parentNode.id.toString()
+
+
+    if (!getText) {
+      setIsTextSelected(false)
+      return
+    }
+
     setIsTextSelected(true)
+    setSelectedText({...selectedText,
+      [getTextCategory]: getText})
     const oRect = window.getSelection().getRangeAt(0).getBoundingClientRect();
-    console.log(oRect)
     setExtractedPosition({     
       x: oRect.x + (oRect.width / 2) - (80 / 2),
-      y: oRect.y + (oRect.height / 2) - (30 * 4 ) ,
+      y: oRect.y + (oRect.height / 2) + (window.scrollY) - (30 * 4 ) ,
       width: oRect.width,
       height: oRect.height,
     })
-
   }
+
+    useEffect(() => {
+    document.addEventListener('mouseup', handleSelection);
+    return () => {
+      document.removeEventListener('mouseup', handleSelection);
+    }
+  }, []);
+
+
   return (
     <>
-    <div className={styles.header} onMouseDown={cancelSelection}>
+    <div className={styles.header}>
         <div className={styles.headerText}>
           Highlight text and select &quot;Extract&quot; for it to be added to
           the search.
         </div>
       </div>
       {isTextSelected && extractedPosition.x && extractedPosition.y && (
-        <SelectedTextBox position={extractedPosition}/>
+        <SelectedTextBox position={extractedPosition} clickHandler={handleExtractClick} />
       )}
       <div className={styles.dataContainer}>
-        <div className={styles.leftContainer} onMouseDown={cancelSelection}>
-        <div className={styles.leftContainer.cardName} onMouseUp={handleSelection} onDoubleClick={handleSelection} >
+        <div className={styles.leftContainer}>
+        <div className={styles.leftContainer.cardName}>
           <p>Card Name: </p>
           <p id={'name'} className={styles.textContatiner}>{cardDetails.name}</p>
         </div>
 
-        <div className={styles.color} onMouseUp={handleSelection}onDoubleClick={handleSelection}>
+        <div className={styles.color}>
           <p>Color: </p>
           <p id={'color'} className={styles.textContatiner}>{cardDetails.colors}</p>
         </div>
 
-        <div className={styles.manaCost} onMouseUp={handleSelection} onDoubleClick={handleSelection}>
+        <div className={styles.manaCost} >
           <p>Mana Cost: </p>
           <p id={'manaCost'} className={styles.textContatiner}>{cardDetails.mana_cost}</p>
         </div>
 
-        <div className={styles.cmc} onMouseUp={handleSelection} onDoubleClick={handleSelection}>
+        <div className={styles.cmc} >
           <p>CMC: </p>
           <p id={'cmc'} className={styles.textContatiner}>{cardDetails.cmc}</p>
         </div>
 
-        <div className={styles.cardType} onMouseUp={handleSelection} onDoubleClick={handleSelection}>
+        <div className={styles.cardType} >
           <p>Card Type: </p>
           <p id={'type'} className={styles.textContatiner}>{cardDetails.type_line}</p>
         </div>
 
-        <div className={styles.set} onMouseUp={handleSelection} onDoubleClick={handleSelection}>
+        <div className={styles.set} >
           <p>Set: </p>
           <p id={'set'} className={styles.textContatiner}>{cardDetails.set.toLocaleUpperCase()}</p>
         </div>
 
-        <div className={styles.text} onMouseUp={handleSelection} onDoubleClick={handleSelection}>
+        <div className={styles.text} >
           <p>Text: </p>
           <p id={'oracle'} className={styles.textContatiner}>{parseSpaceFunction(cardDetails.oracle_text)}</p>
       </div>
  </div>
-        <div className={styles.rightContainer} onMouseDown={cancelSelection}>
+        <div className={styles.rightContainer}>
         <div className={styles.imageContainer}>
           <img src={cardDetails.image_uris.normal}></img>
         </div>
--- the-magic-mirror/src/components/cardTemplate/selectedTextBox.js
+++ the-magic-mirror/src/components/cardTemplate/selectedTextBox.js
@@ -3,9 +3,9 @@ import styles from "./cardTemplate.module.css";
 
 
 // eslint-disable-next-line react/prop-types
-const SelectedTextBox = ({position}) => {
+const SelectedTextBox = ({position, clickHandler}) => {
     return (
-        <p className={styles.selectedTextButton} 
+        <p className={styles.selectedTextButton} onClick={clickHandler}
             style={{
                 // eslint-disable-next-line react/prop-types
                 transform: `translate3d(${position.x}px, ${position.y}px, 0)`
