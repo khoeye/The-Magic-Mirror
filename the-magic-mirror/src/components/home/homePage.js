6885c6d1f80bc4361f941a5e931822a863e46ac6
khoeye <kyle.hoeye@gmail.com>
2025-01-19 16:17:38 -0600

    Add basic redux and resolve issues

--- the-magic-mirror/src/components/home/homePage.js
+++ the-magic-mirror/src/components/home/homePage.js
@@ -1,25 +1,50 @@
-import React from "react"
-import styles from "./home.module.css"
-import Hero from "../hero/hero"
-import CardSearch from "../cardSearch/cardSearch"
+import React from "react";
+import styles from "./home.module.css";
+import Hero from "../hero/hero";
+import CardSearch from "../cardSearch/cardSearch";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+} from "features/counterSlice";
 
 const HomePage = () => {
-    return [
-        <>
-        <div className={styles.headerContainer}>
-            <Hero home={true} advancedSearch={true}/>
-            <div className={styles.headerText}>
-            Magic Mirror lets you choose a card to use as a template for <br></br>
-searches or to build your own advanced search.
-</div>
-        </div>
-        <div className={styles.searchContainer}>
-            <CardSearch></CardSearch>
-        </div>
-            
-            
-        </>
-    ]
-}
 
-export default HomePage
+      const count = useSelector((state) => state.counter.value);
+      const dispatch = useDispatch();
+
+
+  return [
+    <>
+      <div className={styles.headerContainer}>
+        <Hero home={true} advancedSearch={true} />
+        <div className={styles.headerText}>
+          Magic Mirror lets you choose a card to use as a template for <br></br>
+          searches or to build your own advanced search.
+        </div>
+      </div>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+      <div className={styles.searchContainer}>
+        <CardSearch></CardSearch>
+      </div>
+    </>,
+  ];
+};
+
+export default HomePage;
--- the-magic-mirror/src/index.js
+++ the-magic-mirror/src/index.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import CardSearch from './components/cardSearch/cardSearch';
-import HomePage from './components/home/homePage';
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-import CardTemplate from './components/cardTemplate/cardTemplate';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import CardSearch from "./components/cardSearch/cardSearch";
+import HomePage from "./components/home/homePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CardTemplate from "./components/cardTemplate/cardTemplate";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-    },
-    {
-        path: "/cardsearch",
-        element: <CardSearch />
-    },
-    {
-      path: `/template`,
-      element: <CardTemplate />
-    }
-  ]);
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/cardsearch",
+    element: <CardSearch />,
+  },
+  {
+    path: `/template`,
+    element: <CardTemplate />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </React.StrictMode>
-  )
+    </Provider>
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
